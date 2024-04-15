7d62f9d40fa5d344d539f3bfa56c418018fc92da
achintski <achintski@hotmail.com>
2024-04-15 14:57:54 +0800

    till quiz2

--- exercises/modules/modules2.rs
+++ exercises/modules/modules2.rs
@@ -7,12 +7,13 @@
 // Execute `rustlings hint modules2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 mod delicious_snacks {
     // TODO: Fix these use statements
-    use self::fruits::PEAR as ???
-    use self::veggies::CUCUMBER as ???
+    // 注意外界使用需要pub
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
 
     mod fruits {
         pub const PEAR: &'static str = "Pear";
